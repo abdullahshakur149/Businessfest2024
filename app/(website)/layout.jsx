a169a5f7e023eb7ab3285dadef44ad4fd77ca68a
Abdullah Shakur <109961338+abdullahshakur149@users.noreply.github.com>
2024-11-11 00:31:02 +0500

    main

--- app/(website)/layout.jsx
+++ app/(website)/layout.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import "@/app/globals.css";
+import SessionProvider from "@/lib/SessionProvider";
+
 
 const layout = ({children}) => {
   return (
     <html lang='en'>
         <body>
+        <SessionProvider>
     <main>{children}</main>
+    </SessionProvider>
         </body>
     </html>
   )
---
+++ lib/SessionProvider.jsx
@@ -0,0 +1,9 @@
+// lib/SessionProvider.jsx
+"use client";
+import { SessionProvider as NextAuthSessionProvider } from "next-auth/react";
+
+const SessionProvider = ({ children }) => {
+  return <NextAuthSessionProvider>{children}</NextAuthSessionProvider>;
+};
+
+export default SessionProvider;

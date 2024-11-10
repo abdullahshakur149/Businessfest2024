77e1a5fe877c968e23e2ad5360b698dc329385ff
Mansoor khan <107978052+mansoor369@users.noreply.github.com>
2024-11-11 03:24:04 +0500

    .

--- app/Dashboard/page.jsx
+++ app/Dashboard/page.jsx
@@ -4,7 +4,7 @@ import DashboardLayout from '@/components/Dashlayout';
 export default function DashboardHome() {
   return (
     <DashboardLayout>
-      <h1 ></h1>
+      <h1 style={{marginLeft:"10rem"}}>Dashboard</h1>
     </DashboardLayout>
   );
 }

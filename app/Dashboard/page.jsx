5a9fbf04095946ea28ccae38c3f5d57bea571f36
Mansoor khan <107978052+mansoor369@users.noreply.github.com>
2024-11-11 11:58:35 +0500

    .

--- app/Dashboard/page.jsx
+++ app/Dashboard/page.jsx
@@ -2,18 +2,25 @@
 import DashboardLayout from '@/components/Dashlayout';
 import { useSession } from "next-auth/react"
 
+
 export default function DashboardHome() {
   const { data: session, status } = useSession();
 
   return (
     <DashboardLayout>
-      <div className="flex flex-col justify-center items-center h-screen text-center">
-        <h1 className="text-4xl font-bold mb-4 bg-gradient-to-r  from-black/50 via-purple-700 to-cyan-400 ">Businessfest Admin Portal</h1>
+      <div style={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+        textAlign: 'center'
+      }}>
+        <h1 className='text-4xl'>Businessfest admin portal</h1>
         {status === "authenticated" && (
-          <p className="text-lg text-gray-600">Welcome, {session.user.name}!</p>
+          <p>Welcome, {session.user.name}!</p>
         )}
       </div>
     </DashboardLayout>
   );
 }
-6
